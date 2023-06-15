aca0fd797ba0a928161d95bdec9fed32afb38daf
Bassey A. Akan <bassey@HL-DEV-MBL2351.local>
2023-06-15 17:21:46 +0100

    Fix for build

--- src/App.tsx
+++ src/App.tsx
@@ -3,42 +3,6 @@ import { Dashboard, Onboarding, Splash } from './pages';
 import FundAccount from "./pages/fund.page";
 import WithdrawFunds from "./pages/withdraw.page";
 import SendGift from "./pages/send-gift.page";
-import React from 'react';
-
-
-
-// const router  = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <Splash />,
-//   },
-//   {
-//     path: '/home',
-//     element: <Dashboard />
-//   },
-//   {
-//     path: '/',
-//      element: <Navigate to="home"/>
-//   },
-//   {
-//     path: '/onboarding',
-//     element: <Onboarding />
-//   },
-  
-//   {
-//     path: '/fund',
-//     element: <FundAccount />
-//   },
-//   {
-//     path: '/withdraw',
-//     element: <WithdrawFunds />
-//   },
-//   {
-//     path: '/send-gift',
-//     element: <SendGift />
-//   }
-
-// ]);
 
 function App() {
   return (
